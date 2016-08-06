84a2759dfdc2f6ab11afc143a69f7f114f66c194
jotagalabe <jotagalabe@gmail.com>
2016-08-06 16:10:08 -0500

    added comparison between manifolds from subsets of chs to mfxval_pcs

--- dim_reduction/mfxval_pcs.m
+++ dim_reduction/mfxval_pcs.m
@@ -16,10 +16,14 @@
 %                           domain, and comparing these projections to
 %                           those obtained with the PCs computed using all
 %                           the electrodes. This is repeated 'nbr_reps'
-%                           times 
-%   xval_param          : if 'xval_mode' = 'time', length of the folds (s),
+%                           times; 'subsets' will cross-validate btw
+%                           subsets computed using m channels
+%   xval_param          : if 'xval_mode' = 'time', length of the folds (s);
 %                           if 'xval_mode' = 'channels', percentage of
-%                           channels that will be used (0:1)
+%                           channels (0:1) that will be compared to all the
+%                           channels; if 'xval_mode = subsets', percentage
+%                           of channels (0:1) that will be used to compute
+%                           the manifolds
 %   (nbr_reps)          : [100] nbr_repetitions for xval_mode = 'channels', 
 %                           
 % Outputs:
@@ -43,7 +47,7 @@ switch xval_mode
         if nargin == 5
             warning('input arg 4 will be ignored in time mode');
         end
-    case 'channels'
+    case {'channels','subsets'}
         perc_chs            = xval_param;
         all_chs             = double(arrayfun(@(x) x.id(1), bdf.units));
         discard_chs         = setdiff(all_chs, neural_chs);
@@ -62,11 +66,15 @@ nbr_neural_chs              = numel(neural_chs);
 switch xval_mode
     % --------------------------------------------------
     case 'time'
+        
         error('not implemented yet')
+        
+        
     % --------------------------------------------------
     case 'channels'
+        
         % preallocate matrix for storing random channels
-        chs                 = zeros(nbr_reps,...
+        chs_1               = zeros(nbr_reps,...
                                 round(nbr_neural_chs*perc_chs));
         % preallocate matrix for storing CC correlations
         R_CC                = zeros(nbr_pcs_comp,nbr_reps);
@@ -81,7 +89,8 @@ switch xval_mode
         % do PCA of all the selected channels
         dim_red_FR          = dim_reduction(smoothed_FR,'pca',discard_chs);
                         
-        disp(['computing CCs of subsets of projections from' perc_chs ' % of the electrodes...'])
+        disp(['computing CCs of subsets of projections from ' num2str(perc_chs*100)...
+            ' % of the electrodes...']);
         for i = 1:nbr_reps
             % randomly choose a subset of perc_chs
             chs(i,:)        = datasample(1:nbr_neural_chs,...
@@ -115,6 +124,61 @@ switch xval_mode
 %         % stats.stats_Proc    = stats_Proc;
 %         stats.mean_R2_Proc  = mean(R2_Proc,2);
 %         stats.std_R2_Proc   = std(R2_Proc,0,2);
+
+
+    % --------------------------------------------------
+    case 'subsets'
+
+        % preallocate matrix for storing random channels
+        chs_1               = zeros(nbr_reps,...
+                                round(nbr_neural_chs*perc_chs));
+        chs_2               = zeros(nbr_reps,...
+                                round(nbr_neural_chs*perc_chs));
+                            
+        % preallocate matrix for storing CC correlations
+        R_CC                = zeros(nbr_pcs_comp,nbr_reps);
+        R2_CC               = zeros(nbr_pcs_comp,nbr_reps);
+
+        % smooth the FRs
+        smoothed_FR         = gaussian_smoothing2(bdf,'sqrt',0.02,0.05); %#ok<AGROW>                       
+
+        disp(['Computing CCs btw pair of projections onto random subsets of ' ...
+            num2str(perc_chs*100) ' % of the electrodes...']);
+        
+        
+        for i = 1:nbr_reps
+            % randomly choose the first subset of perc_chs
+            chs_1(i,:)      = datasample(1:nbr_neural_chs,...
+                            round(nbr_neural_chs*perc_chs),'Replace',false);
+            chs_2(i,:)      = datasample(1:nbr_neural_chs,...
+                            round(nbr_neural_chs*perc_chs),'Replace',false);
+        
+            % make sure they are not the same
+            if chs_1 == chs_2, warning('two subsets of electrodes are identical!!!'); end
+            
+            % discard the channels when do not want (for making the random
+            % subsets) ...
+            discard_chs_1   = setdiff(all_chs,sort(chs_1(i,:)));
+            discard_chs_2   = setdiff(all_chs,sort(chs_2(i,:)));
+            % ...and do pca
+            dim_red_FR_1    = dim_reduction(smoothed_FR,'pca',discard_chs_1);
+            dim_red_FR_2    = dim_reduction(smoothed_FR,'pca',discard_chs_2);
+            
+            % compare the dynamics of the projections using all the
+            % channels, and the randonmly chosen subset of percentage of channels
+            [~,~,R_CC(:,i),U,V,stats_CC{i}] = canoncorr(dim_red_FR_1.scores(:,1:nbr_pcs_comp),...
+                            dim_red_FR_2.scores(:,1:nbr_pcs_comp)); %#ok<AGROW>
+            R2_CC(:,i)      = CalculateR2(U,V);
+
+           % append statistics
+            stats.R_CC          = R_CC;
+            stats.mean_R_CC     = mean(R_CC,2);
+            stats.std_R_CC      = std(R_CC,0,2);
+            stats.stats_CC      = stats_CC;
+            stats.R2_CC         = R2_CC;
+            stats.mean_R2_CC    = mean(R2_CC,2);
+            stats.std_R2_CC     = std(R2_CC,0,2); 
+        end
 end
 
 % -------------------------------------------------------------------------
@@ -122,6 +186,7 @@ end
 switch xval_mode
     case 'time'
         error('not implemented yet')        
+    
     case 'channels'
         % CCs of Canonical correlation
         figure,plot(R_CC,'color',[.6 .6 .6]), hold on
@@ -139,21 +204,15 @@ switch xval_mode
         xlabel('projection nbr.'),ylabel('canonical correlation')
         title(['CCs btw projections from ' num2str(perc_chs*100) ...
             ' % of the electrodes vs. all electrodes'])
+    
+    case 'subsets'
+        figure,plot(R_CC,'color',[.6 .6 .6]), hold on
+        plot(stats.mean_R_CC,'linewidth',4,'color','k')
+        plot(stats.mean_R_CC+stats.std_R_CC,'linewidth',4,'color','k','linestyle','-.')
+        plot(stats.mean_R_CC-stats.std_R_CC,'linewidth',4,'color','k','linestyle','-.')
         
-%         % R2 Procrustes vs R2 CC
-%         figure,plot(R2_Proc,'color',[.6 .6 .6]), hold on
-%         plot(R2_CC,'color',[1 .6 0]), hold on
-% 
-%         plot(stats.mean_R2_Proc,'linewidth',4,'color','k')
-%         plot(stats.mean_R2_Proc+stats.std_R2_Proc,'linewidth',4,'color','k','linestyle','-.')
-%         plot(stats.mean_R2_Proc-stats.std_R2_Proc,'linewidth',4,'color','k','linestyle','-.')
-%         
-%         plot(stats.mean_R2_CC,'linewidth',4,'color','r')
-%         plot(stats.mean_R2_CC+stats.std_R2_CC,'linewidth',4,'color','r','linestyle','-.')
-%         plot(stats.mean_R2_CC-stats.std_R2_CC,'linewidth',4,'color','r','linestyle','-.')
-%         
-%         set(gca,'TickDir','out'),set(gca,'FontSize',14), ylim([0 1])
-%         xlabel('projection nbr.'),ylabel('R2 after Procrustes (black) vs. CC (red)')
-%         title(['preservation of neural projections after loosing ' num2str((1-perc_chs)*100) ...
-%             ' % of the channels'])
+        set(gca,'TickDir','out'),set(gca,'FontSize',14), ylim([0 1])
+        xlabel('projection nbr.'),ylabel('canonical correlation')
+        title(['CCs btw pair of projections onto random subsets of ' num2str(perc_chs*100)...
+            ' % of the electrodes'])
 end
