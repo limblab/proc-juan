6c8976455da810e73147aff319d26403ed38cec6
jotagalabe <jotagalabe@gmail.com>
2016-08-14 15:34:54 -0500

    mfxval_pcs: added crossvalidation over time --need to clean out the 'subsets' xval mode

--- dim_reduction/mfxval_pcs.m
+++ dim_reduction/mfxval_pcs.m
@@ -32,14 +32,27 @@
 %                           electrodes vs. the subsampled sets
 %
 %
+% Notes:
+%   - There is some stuff in subsets that needs to be deleted
+%
 
 function stats = mfxval_pcs( bdf, neural_chs, xval_mode, xval_param, varargin )
 
 
 % number of PCs that will be compared across the total and subsampled
-% datasets 
+% datasets --could be turn into a param
 nbr_pcs_comp                = 20;
+% bin size (s) --could be turn into a param
+bin_size                    = 0.02;
+% SD Gaussian kernel (s) --could be turn into a param
+gauss_SD                    = 0.05;
 
+
+% some preliminary definitions
+all_chs                     = double(arrayfun(@(x) x.id(1), bdf.units));
+discard_chs                 = setdiff(all_chs, neural_chs);
+
+% -------------------------------------------------------------------------
 % read input parameters
 switch xval_mode
     case 'time'
@@ -49,8 +62,6 @@ switch xval_mode
         end
     case {'channels','subsets'}
         perc_chs            = xval_param;
-        all_chs             = double(arrayfun(@(x) x.id(1), bdf.units));
-        discard_chs         = setdiff(all_chs, neural_chs);
         if nargin == 5
             nbr_reps        = varargin{1};
         else
@@ -67,8 +78,89 @@ switch xval_mode
     % --------------------------------------------------
     case 'time'
         
-        error('not implemented yet')
+        % get nbr of folds
+        if isfield(bdf,'emg')
+            t_end           = bdf.emg.data(end,1);
+            t_start         = bdf.emg.data(1,1);
+        elseif isfield(bdf,'force')
+            t_end           = bdf.force.data(end,1);
+            t_start         = bdf.force.data(1,1);
+        elseif isfield(bdf,'pos')
+            t_end           = bdf.pos(end,1);
+            t_start         = bdf.pos(1,1);
+        else
+            error('BDF does not have emg, force or pos fields');
+        end
+        nbr_folds           = floor((t_end-t_start)/fold_length);
         
+        
+        % preallocate matrices for scoring the stats
+        R_CC                = zeros(nbr_pcs_comp,nbr_folds);
+        R2_CC               = zeros(nbr_pcs_comp,nbr_folds);
+        R2_raw              = zeros(nbr_pcs_comp,nbr_folds);
+        
+        % smooth firing rates and add them to the binned_data struct
+        [smoothed_FR, binned_data] = gaussian_smoothing(bdf,'sqrt',bin_size,gauss_SD);
+        dim_red_FR          = dim_reduction(smoothed_FR,'pca',discard_chs);
+        
+        
+        disp(['Doing mfxval of the projections onto PCs over time...']);
+        % do for each fold
+        for f = 0:nbr_folds-1
+            
+            % 1. define the fold that will be left out
+            data_out_start  = t_start + fold_length*f;
+            data_out_end    = data_out_start + fold_length;
+            
+            % 2. get the training data window (will compute neural
+            % primitives from here and test them on the synergies from the
+            % entire data window
+            [train_data, ~] = splitBinnedData( binned_data, ...
+                                        data_out_start, data_out_end );
+                                    
+            % create a smoothed_FR array (with the time vector in col 1),
+            % so it is compatible with dim_reduction
+            train_smoothed_FR = [train_data.timeframe, train_data.smoothedspikerate];
+                                    
+            % 3. do PCA on train_data to compare its neural primitives to
+            % those of the corresponding window obtained from 
+            train_dim_red_FR = dim_reduction(train_smoothed_FR, ...
+                                'pca',discard_chs);
+                            
+            % 4. get the neural primitives obtained from the entire dataset
+            % that correspond to the primitives in train_dim_red_FR
+            indx_scores_all = round(train_data.timeframe/bin_size);
+            
+            % ToDo: delete this dirty check
+            if find( (dim_red_FR.t(indx_scores_all) - train_dim_red_FR.t)>0 )
+                figure,plot(dim_red_FR.t(indx_scores_all) - train_dim_red_FR.t)
+            end
+            
+            scores_all      = dim_red_FR.scores(indx_scores_all,1:nbr_pcs_comp);
+            scores_train    = train_dim_red_FR.scores(:,1:nbr_pcs_comp);
+            
+            % 5. see how similiar the projections are 
+            [~,~,R_CC(:,f+1),U,V,stats_CC{f+1}] = canoncorr(scores_all,scores_train);
+            R2_CC(:,f+1)    = CalculateR2(U,V);
+            
+            R2_raw(:,f+1)   = CalculateR2(scores_all,scores_train);
+            
+            clear train_data train_smoothed_FR train_dim_red_FR scores_all scores_train;
+        end
+        
+        % append statistics
+        stats.R_CC          = R_CC;
+        stats.mean_R_CC     = mean(R_CC,2);
+        stats.std_R_CC      = std(R_CC,0,2);
+        stats.stats_CC      = stats_CC;
+        stats.R2_CC         = R2_CC;
+        stats.mean_R2_CC    = mean(R2_CC,2);
+        stats.std_R2_CC     = std(R2_CC,0,2);
+        
+        stats.R2_raw        = R2_raw;
+        stats.mean_R2_raw   = mean(R2_raw,2);
+        stats.std_R2_raw     = std(R2_raw,0,2);
+  
         
     % --------------------------------------------------
     case 'channels'
@@ -85,7 +177,7 @@ switch xval_mode
 
                         
         % smooth the FRs
-        smoothed_FR         = gaussian_smoothing2(bdf,'sqrt',0.02,0.05); %#ok<AGROW>                       
+        smoothed_FR         = gaussian_smoothing2(bdf,'sqrt',bin_size,gauss_SD); %#ok<AGROW>                       
         % do PCA of all the selected channels
         dim_red_FR          = dim_reduction(smoothed_FR,'pca',discard_chs);
                         
@@ -140,7 +232,7 @@ switch xval_mode
         R2_CC               = zeros(nbr_pcs_comp,nbr_reps);
 
         % smooth the FRs
-        smoothed_FR         = gaussian_smoothing2(bdf,'sqrt',0.02,0.05); %#ok<AGROW>                       
+        smoothed_FR         = gaussian_smoothing2(bdf,'sqrt',bin_size,gauss_SD); %#ok<AGROW>                       
         
         disp(['Computing CCs btw pair of projections onto random subsets of ' ...
             num2str(perc_chs*100) ' % of the electrodes...']);
@@ -209,7 +301,21 @@ end
 % plot
 switch xval_mode
     case 'time'
-        error('not implemented yet')        
+        figure,subplot(121),
+        plot(R_CC,'color',[.6 .6 .6]), hold on
+        plot(stats.mean_R_CC,'linewidth',4,'color','k')
+        plot(stats.mean_R_CC+stats.std_R_CC,'linewidth',4,'color','k','linestyle','-.')
+        plot(stats.mean_R_CC-stats.std_R_CC,'linewidth',4,'color','k','linestyle','-.')
+        set(gca,'TickDir','out'),set(gca,'FontSize',14), ylim([0 1])
+        xlabel('projection nbr.'),ylabel('canonical correlation')
+        
+        subplot(122),
+        plot(R2_raw,'color',[1 .6 0]), hold on
+        plot(stats.mean_R2_raw,'linewidth',4,'color','r')
+        plot(stats.mean_R2_raw+stats.std_R2_raw,'linewidth',4,'color','r','linestyle','-.')
+        plot(stats.mean_R2_raw-stats.std_R2_raw,'linewidth',4,'color','r','linestyle','-.')
+        set(gca,'TickDir','out'),set(gca,'FontSize',14), ylim([0 1])
+        xlabel('projection nbr.'),ylabel('R2 projections')
     
     case 'channels'
         % CCs of Canonical correlation
