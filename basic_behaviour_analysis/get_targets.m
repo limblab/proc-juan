4b1efd811c00322210afe288fae409b4854521d5
jotagalabe <jotagalabe@gmail.com>
2016-08-28 01:30:20 -0500

    get_targets.m: fixed an issue related to only having one repetition of a not-specified target

--- basic_behaviour_analysis/get_targets.m
+++ basic_behaviour_analysis/get_targets.m
@@ -11,13 +11,19 @@
 %
 % Ouputs:
 %   nbr_targets         : number of targets
-%   target_coord        : target coordinates (ULx ULy Width Height) for
-%                           each target
-%   target_id           : ID of each of the targets
+%   target_coord        : 2D matrix with each row being the coordinates of
+%                           each of the targets (ULx ULy LRx LRy)
+%   target_nbr          : 1D matrix with the sequence in which the targets
+%                           appeared. Targets are named 1,2, ..., n, where
+%                           n is the number of rows in target_coord
+%   target_coord_mtlb   : 2D matrix with target coordinates (ULx ULy Width
+%                           Height) for each target --this is the format
+%                           matlab likes for plotting them with rectangle
 %
 %
 
-function [nbr_targets, target_coord, target_id] = get_targets( binned_data, task, varargin )
+function [nbr_targets, target_coord, target_nbr, varargout] = ...
+            get_targets( binned_data, task, varargin )
 
 if nargin == 3
     plot_yn             = varargin{1};
@@ -25,24 +31,33 @@ else
     plot_yn             = false;
 end
 
+% for compatibility with different wrapper functions
+if iscell(task)
+    task_name           = task{1};
+else
+    task_name           = task;
+end
+
+
 % find targets
-switch task{1}
+switch task_name
     case {'wf','iso','spr','iso8','wm'}
         targets         = unique(binned_data.trialtable(:,2:5),'rows');
     case 'ball'
         targets         = [-1 1 1 -1]; % arbitrarily create a target centered in [0 0]
     case {'mg','mg-pt'}
-%         % old and new datasets have different trial tables, but we can
-%         % distinguish them based on their number of columns. The old trial
-%         % table only has 7 columns, and the new one has 12. Unfortunately
-%         % in an intermediate time, the target coordinates in the 12-column
-%         % trial table were [-1 -1 -1 -1]
+        % old and new datasets have different trial tables, but we can
+        % distinguish them based on their number of columns. The old trial
+        % table only has 7 columns, and the new one has 12. Unfortunately
+        % in an intermediate time, the target coordinates in the 12-column
+        % trial table were always [-1 -1 -1 -1]
 %         nbr_cols_mg     = size(binned_data.trialtable,2);
 %         switch nbr_cols_mg
 %             case 12
         targets = unique(binned_data.trialtable(:,7:10),'rows');
-% datasets from Theo don't have target coordinates, it's a
-% vector with -1's.
+        % datasets from Theo don't have target coordinates, it's a vector
+        % with -1's. The code below just converts them to some coordinates
+        % that make sense for plotting
         if targets(1,:) == [-1 -1 -1 -1]
             nbr_targets = numel(find(unique(binned_data.trialtable(:,6))>=0));
             targets = zeros(nbr_targets,4);
@@ -89,21 +104,47 @@ rect_coord(rect_coord(:,4) == 0,:) = [];
 % create vector with target ID for each trial
 
 nbr_trials              = size(binned_data.trialtable,1);
-target_id               = zeros(1,nbr_trials);
+target_nbr              = zeros(1,nbr_trials);
 
-switch task{1}
+switch task_name
     case {'wf','iso','spr','iso8','wm'}
-        target_id       = binned_data.trialtable(:,10);
+        
+        target_indxs    = arrayfun(@(x,y,z,w) find(binned_data.trialtable(:,2)==x & binned_data.trialtable(:,3)==y & binned_data.trialtable(:,4)==z & binned_data.trialtable(:,5)==w), ...
+            targets(:,1), targets(:,2), targets(:,3), targets(:,4),'UniformOutput',false);
+        % ToDo: double check this DIRTY FIX
+        % --it seems that sometimes there is one target that is mistakenly
+        % labelled, and it's only one. The code will get rid of it
+        indx_discard    = cellfun( @(x) find(length(x)==1), target_indxs, 'UniformOutput', false ); 
+        target_indxs(find(cellfun(@(x) ~isempty(x), indx_discard))) = [];
+        
+        % update the number of targets
+        nbr_targets     = length(target_indxs);
+        
+        % fill a vector of target numbers with a scalar that identifies the
+        % target (>= 0)
+        target_nbr      = nan(1,sum(cellfun(@(x) length(x), target_indxs)));
+        for i = 1:length(target_indxs)
+            target_nbr(target_indxs{i}) = i-1;
+        end
+        
     case 'ball'
-        target_id       = zeros(1,nbr_trials);
+        target_nbr      = zeros(1,nbr_trials);
     case {'mg','mg-pt'}
-        target_id       = binned_data.trialtable(:,6);
+        target_nbr      = binned_data.trialtable(:,6);
 end
 
 
 % -------------------------------------------------------------------------
 % return variables
-target_coord            = rect_coord;
+if exist('indx_discard','var')
+    target_coord        = targets(setdiff(1:size(targets,1),...
+                            find(cellfun(@(x) ~isempty(x), indx_discard))),:);
+else
+    target_coord        = targets;
+end
+if nargout == 4
+    varargout{1}        = rect_coord;
+end
 
 
 % -------------------------------------------------------------------------
