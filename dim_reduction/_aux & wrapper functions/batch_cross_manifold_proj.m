2e2c2c9ddb5cc2e3f092cbfd988bd56b490d2887
jotagalabe <jotagalabe@gmail.com>
2016-08-30 22:19:53 -0500

    minor changes to batch_cross_manifold_projs, to try to understand how to align the manifolds...

--- dim_reduction/_aux & wrapper functions/batch_cross_manifold_proj.m
+++ dim_reduction/_aux & wrapper functions/batch_cross_manifold_proj.m
@@ -57,9 +57,11 @@ for i = 1:meta_info.nbr_monkeys
 
         % compare projections onto each of the dimensions of the within
         % task and the across task manifold in two cases:
+        %
         %   1) ranking the eigenvectors in the way that yielded the highest
         %   dimensional, non-orthogonal manifolds ('pc_projs')
         %   2) using the alternative eigenvector ranking ('pc_projs_pair2')
+        %
         [pc_projs, pc_projs_pair2] = transform_and_compare_dim_red_data_all_tasks( ...
                             datasets{dtst}.dim_red_FR, ...
                             datasets{dtst}.smoothed_FR, ...
@@ -126,6 +128,8 @@ end
 
 
 
+
+
 % -------------------------------------------------------------------------
 % Some checks to see if the dimension that defines the smallest angle also
 % gives the maximum cross-task projection similarity
@@ -134,50 +138,78 @@ for d = 1:length(datasets)
     
     for p = 1:length(proj_results{d}.min_manifold_angle_pair)
     
+        % retrieve correlation
+        corr_min_angle      = proj_results{d}.min_manifold_angle_pair(p).r;
+        corr_max_angle      = proj_results{d}.max_manifold_angle_pair(p).r;
+        
         % find maximum correlation when projecting on other dimensions
         [max_r_other_min_mani_angle, eigenv_max_r_other_min_mani_angle] = ...
                     max(proj_results{d}.min_manifold_angle_pair(p).r_other_projs);
         [max_r_other_max_mani_angle, eigenv_max_r_other_max_mani_angle] = ...
                     max(proj_results{d}.max_manifold_angle_pair(p).r_other_projs);
-
-        % compare this max correlation to the correlation onto the direction that
-        % defines the smallest angle
-        indx_higher_r_than_corr_eigenv_min_mani = find( max_r_other_min_mani_angle > ...
-            proj_results{d}.min_manifold_angle_pair(p).r );
-        indx_higher_r_than_corr_eigenv_max_mani = find( max_r_other_max_mani_angle > ...
-                    proj_results{d}.max_manifold_angle_pair(p).r );
-
-        figure,
-        subplot(121),hold on
-        plot(proj_results{d}.min_manifold_angle_pair(p).r,'linewidth',2,'color','b')
-        plot(proj_results{d}.max_manifold_angle_pair(p).r,'linewidth',2,'color','k')
-        plot(max_r_other_min_mani_angle,'linewidth',2,'color','c')
-        plot(max_r_other_max_mani_angle,'linewidth',2,'color',[.6 .6 .6])
-        legend('min mani angle','max mani angle','opt other min mani','opt other max mani')
-        legend boxoff
-        set(gca,'Tickdir','out'),set(gca,'FontSize',14)
-        xlim([0 10])
-        title([meta_info.tasks_per_session{d}{proj_results{d}.min_manifold_angle_pair(p).within_task}, ...
-            ' vs. ' meta_info.tasks_per_session{d}{proj_results{d}.min_manifold_angle_pair(p).across_task}]);
+                
+        % find maximum correlation across all these 4 conditions, for each
+        % dimension
+        aux_r = [corr_min_angle; corr_max_angle; max_r_other_min_mani_angle; max_r_other_max_mani_angle];
+        [max_r, pair_max_r] = max(aux_r,[],1);
         
-        subplot(122),hold on
-        plot(proj_results{d}.min_manifold_angle_pair(p).comp_nbr(:,2),'linewidth',2,'color','b')
-        plot(proj_results{d}.max_manifold_angle_pair(p).comp_nbr(:,2),'linewidth',2,'color','k')
-        plot(eigenv_max_r_other_min_mani_angle,'linewidth',2,'color','c')
-        plot(eigenv_max_r_other_max_mani_angle,'linewidth',2,'color',[.6 .6 .6])
-        legend('min mani angle','max mani angle','opt other min mani','opt other max mani')
-        legend boxoff
+        % plot correlations
+        figure,imagesc(aux_r), colorbar
         set(gca,'Tickdir','out'),set(gca,'FontSize',14)
-        xlim([0 10])
+        set(gca,'YTick',1:4), set(gca,'YTickLabelRotation',45)
+%        set(gca,'YTickLabel',{'other max r max','other max r min','max mani ang','min mani ang'})
+        set(gca,'YTickLabel',{'min mani ang','max mani ang','other max r min','other max r max'})
+        hold on, plot(pair_max_r,'linestyle','none','color','w','marker','.','markersize',24)
+        title([datasets{d}.monkey ' - ' angle_results.data{d}.angles_manifolds.pair_min_angle{p}{1} ...
+            ' vs ' angle_results.data{d}.angles_manifolds.pair_min_angle{p}{2}]);
+        
+        
+%         % compare this max correlation to the correlation onto the direction that
+%         % defines the smallest angle
+%         indx_higher_r_than_corr_eigenv_min_mani = find( max_r_other_min_mani_angle > ...
+%             proj_results{d}.min_manifold_angle_pair(p).r );
+%         indx_higher_r_than_corr_eigenv_max_mani = find( max_r_other_max_mani_angle > ...
+%                     proj_results{d}.max_manifold_angle_pair(p).r );
+
+%         figure,
+%         subplot(121),hold on
+%         plot(proj_results{d}.min_manifold_angle_pair(p).r,'linewidth',2,'color','b')
+%         plot(proj_results{d}.max_manifold_angle_pair(p).r,'linewidth',2,'color','k')
+%         plot(max_r_other_min_mani_angle,'linewidth',2,'color','c')
+%         plot(max_r_other_max_mani_angle,'linewidth',2,'color',[.6 .6 .6])
+%         legend('min mani angle','max mani angle','opt other min mani','opt other max mani')
+%         legend boxoff
+%         set(gca,'Tickdir','out'),set(gca,'FontSize',14)
+%         xlim([0 10])
+%         title([meta_info.tasks_per_session{d}{proj_results{d}.min_manifold_angle_pair(p).within_task}, ...
+%             ' vs. ' meta_info.tasks_per_session{d}{proj_results{d}.min_manifold_angle_pair(p).across_task}]);
+%         
+%         subplot(122),hold on
+%         plot(proj_results{d}.min_manifold_angle_pair(p).comp_nbr(:,2),'linewidth',2,'color','b')
+%         plot(proj_results{d}.max_manifold_angle_pair(p).comp_nbr(:,2),'linewidth',2,'color','k')
+%         plot(eigenv_max_r_other_min_mani_angle,'linewidth',2,'color','c')
+%         plot(eigenv_max_r_other_max_mani_angle,'linewidth',2,'color',[.6 .6 .6])
+%         legend('min mani angle','max mani angle','opt other min mani','opt other max mani')
+%         legend boxoff
+%         set(gca,'Tickdir','out'),set(gca,'FontSize',14)
+%         xlim([0 10])
     end
 end
     
-% -------------------------------------------------------------------------
-% Plot to compare the dimension that gives the smallest angle between manifolds
-% and the dimensions that gives the most correlated cross-task projection
-for d = 1:length(datasets)
-   
-    figure,hold on
-    plot(proj_results{d}.min_manifold_angle_pair.comp_nbr(:,2),'b')
-    plot(proj_results{d}.max_manifold_angle_pair.comp_nbr(:,2),'r')
-end
+
+
+
+% % -------------------------------------------------------------------------
+% % Plot to compare the dimension that gives the smallest angle between manifolds
+% % and the dimensions that gives the most correlated cross-task projection
+% for d = 1:length(datasets)
+%     for p = 1:length(proj_results{d}.min_manifold_angle_pair)
+%         figure,hold on
+%         plot(proj_results{d}.min_manifold_angle_pair(p).comp_nbr(:,2),'k')
+%         plot(proj_results{d}.max_manifold_angle_pair(p).comp_nbr(:,2),'r')
+% 
+%         % ToDo..
+%         
+%         set(gca,'Tickdir','out'),set(gca,'FontSize',14)
+%     end
+% end
