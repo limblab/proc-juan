62edd6bd2a0532d93ce810b68e88f2d7c58cc421
jotagalabe <jotagalabe@gmail.com>
2016-09-03 13:17:02 -0500

    concatenate_single_trials.m: a few minor improvements

--- aux_BDF_bin_functions/concatenate_single_trials.m
+++ aux_BDF_bin_functions/concatenate_single_trials.m
@@ -44,14 +44,14 @@ for t = 1:length(single_trial_data.target)
     
     % 3) the dimensionally-reduced FRs --if present
     if isfield(single_trial_data.target{t}.neural_data,'dim_red')
-        aux             = single_trial_data.target{t}.neural_data.dim_red.scores;
+        aux             = single_trial_data.target{t}.neural_data.dim_red.st_scores;
         conc            = zeros(size(aux,1)*size(aux,3),size(aux,2));    
         for d = 1:size(aux,2)
             sq_conc     = reshape( squeeze(aux(:,d,:)), [], 1 );
             conc(:,d)   = sq_conc;
         end
         
-        single_trial_data.target{t}.neural_data.dim_red.conc_scores = conc;
+        single_trial_data.target{t}.neural_data.dim_red.scores = conc;
     end
     
     
@@ -76,7 +76,7 @@ for t = 1:length(single_trial_data.target)
             conc(:,d)   = sq_conc;
         end
         
-        single_trial_data.target{t}.emg_data.dim_red.conc_scores = conc;
+        single_trial_data.target{t}.emg_data.dim_red.scores = conc;
     end
     
     
@@ -109,6 +109,26 @@ for t = 1:length(single_trial_data.target)
     single_trial_data.target{t}.conc_t = 0:single_trial_data.target{t}.bin_size:...
         (size(single_trial_data.target{t}.neural_data.conc_fr,1)-1)*...
         single_trial_data.target{t}.bin_size;
+    % add concatenated bin indexes
+    single_trial_data.target{t}.bin_indx = reshape(single_trial_data.target{t}.bin_indx_p_trial,1,[]);
     
+    % overwrite the time vector associated to the "neural scores" keeping
+    % its integrity
+    if isfield(single_trial_data.target{t}.neural_data,'dim_red')
+    
+        aux_indx_trans      = find(diff(single_trial_data.target{t}.neural_data.dim_red.t) > ...
+                                single_trial_data.target{t}.bin_size*1.1);
+        aux_indx_trans      = [1; aux_indx_trans+1];
+        indx_dim_red_neural_t = zeros( size(single_trial_data.target{t}.neural_data.dim_red.st_scores,3),...
+                                size(single_trial_data.target{t}.neural_data.dim_red.st_scores,1) );
+        for ii = 1:length(aux_indx_trans)
+            indx_dim_red_neural_t(ii,:) = aux_indx_trans(ii) : aux_indx_trans(ii) + ...
+                                size(single_trial_data.target{t}.neural_data.dim_red.st_scores,1) - 1;
+        end
+        indx_dim_red_neural_t = sort(reshape(indx_dim_red_neural_t,1,[]));
+
+%         single_trial_data.target{t}.neural_data.dim_red.t = ...
+%             single_trial_data.target{t}.neural_data.dim_red.t(indx_dim_red_neural_t,1);
+    end
     
 end
