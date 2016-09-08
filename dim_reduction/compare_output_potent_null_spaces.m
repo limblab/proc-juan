70223930ebfc0d09141f4f6d8fd4e2ba54f3e0c4
jotagalabe <jotagalabe@gmail.com>
2016-09-08 01:56:34 -0500

    compare_output_potent_null_spaces: fcn to compute angles between potent and null spaces

---
+++ dim_reduction/compare_output_potent_null_spaces.m
@@ -0,0 +1,47 @@
+%
+% Compare output potent and null spaces of input-output (e.g., neurons to
+% emg or to force) linear models (with one single bin). The function
+% compares all possible pairs of models.
+%
+%   function comp_opn_spaces = compare_output_potent_null_spaces( opn_spaces )
+%
+%
+% Input:
+%   opn_spaces          : struct generated with output_potent_null_spaces,
+%                           with each field being a different task
+%
+% Output:
+%   comp_opn_spaecs     : struct with comparison results
+%
+%
+
+function comp_opn_spaces = compare_output_potent_null_spaces( opn_spaces ) 
+
+
+% get nbr of tasks
+nbr_bdfs                = length(opn_spaces);
+% and all possible combinations
+comb_bdfs               = nchoosek(1:nbr_bdfs,2);
+nbr_comb_bdfs           = size(comb_bdfs,1);
+
+
+% preallocate matrices to store the results
+princ_ang_V_potent      = zeros(nbr_comb_bdfs,size(opn_spaces(1).V_potent,2));
+princ_ang_V_null        = zeros(nbr_comb_bdfs,size(opn_spaces(1).V_null,2));
+
+% do for all combinations
+for i = 1:nbr_comb_bdfs
+    
+    % compare potent spaces
+    princ_ang_V_potent(i,:) = principal_angles( opn_spaces(comb_bdfs(i,1)).V_potent, ...
+                            opn_spaces(comb_bdfs(i,2)).V_potent );
+    
+    % and null spaces
+    princ_ang_V_null(i,:) = principal_angles( opn_spaces(comb_bdfs(i,1)).V_null, ...
+                            opn_spaces(comb_bdfs(i,2)).V_null );
+end
+
+
+% return variables
+comp_opn_spaces.princ_ang_V_potent  = princ_ang_V_potent;
+comp_opn_spaces.princ_ang_V_null    = princ_ang_V_null;
