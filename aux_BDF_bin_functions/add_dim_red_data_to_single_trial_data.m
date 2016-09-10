b97754ea3ae8e225703a8a5da64e1a8f81b75f46
jotagalabe <jotagalabe@gmail.com>
2016-09-09 20:53:53 -0500

    add_dim_red_data_to_single_trial_data.m: fixed minor bugs & some other improvements

--- aux_BDF_bin_functions/add_dim_red_data_to_single_trial_data.m
+++ aux_BDF_bin_functions/add_dim_red_data_to_single_trial_data.m
@@ -56,90 +56,134 @@ else
     emg_data_yn         = false;
 end
 
+% nbr of vars to add (1/2)
+vars_to_add             = [neural_data_yn, emg_data_yn];
+indx_vars_to_add        = find(vars_to_add);
 
 % -------------------------------------------------------------------------
-% add neural data
-if neural_data_yn
-    
-    nbr_targets         = length(single_trial_data.target) - 1;
-    nbr_bins            = size(single_trial_data.target{1}.bin_indx_p_trial,1);
-    nbr_chs             = length(neural_data.eigen);
-    
-    % -----------------------------------
-    % 1) add scores (projections onto PCs) for each target
-    
-    for t = 1:nbr_targets
-        
-        % individual trials
-        nbr_trials_this_tgt = size(single_trial_data.target{t}.bin_indx_p_trial,2);
-        
-        single_trial_data.target{t}.neural_data.dim_red.st_scores = ...
-            zeros(nbr_bins,nbr_chs,nbr_trials_this_tgt);
+% add data
 
-        for r = 1:nbr_trials_this_tgt
-            single_trial_data.target{t}.neural_data.dim_red.st_scores(:,:,r) = ...
-                neural_data.scores(single_trial_data.target{t}.bin_indx_p_trial(:,r),:);
-        end
-            
-        % all concatenated trials
-        single_trial_data.target{t}.neural_data.dim_red.scores = ...
-            neural_data.scores(single_trial_data.target{t}.bin_indx,:); 
-%         single_trial_data.target{t}.neural_data.dim_red.t = ...
-%             neural_data.t(single_trial_data.target{t}.bin_indx);
+for v = 1:length(indx_vars_to_add)
+    
+    % if it is the neural data that we want to add
+    if indx_vars_to_add(v) == 1
+       
+        this_var        = 'neural_data';
+        nbr_chs         = length(neural_data.eigen);
         
-        % add mean and SD scores
-        single_trial_data.target{t}.neural_data.dim_red.st_scores_mn = ...
-            mean(single_trial_data.target{t}.neural_data.dim_red.st_scores,3);
-        single_trial_data.target{t}.neural_data.dim_red.st_scores_sd = ...
-            std(single_trial_data.target{t}.neural_data.dim_red.st_scores,0,3);
+    % if it is the emg data that we want to add
+    elseif indx_vars_to_add(v) == 2
+
+        this_var        = 'emg_data';
+        nbr_chs         = length(emg_data.chs);
     end
     
+    % get some meta info
+    nbr_targets         = length(single_trial_data.target) - 1;
+    nbr_bins            = size(single_trial_data.target{1}.bin_indx_p_trial,1);
+    
+
+    % -----------------------------------
+    % 1) add scores (projections onto PCs) for each target
+
+    for t = 1:nbr_targets
+
+        % individual trials
+        nbr_trials_this_tgt = size(single_trial_data.target{t}.bin_indx_p_trial,2);
+
+        for r = 1:nbr_trials_this_tgt
+            switch this_var
+                case 'neural_data'
+                    single_trial_data.target{t}.(this_var).dim_red.st_scores = ...
+                        zeros(nbr_bins,nbr_chs,nbr_trials_this_tgt);
+                    
+                    single_trial_data.target{t}.(this_var).dim_red.st_scores(:,:,r) = ...
+                        neural_data.scores(single_trial_data.target{t}.bin_indx_p_trial(:,r),:);
+                case 'emg_data'
+                    single_trial_data.target{t}.(this_var).dim_red.st_scores = ...
+                        zeros(nbr_bins,size(emg_data.w,2),nbr_trials_this_tgt);
+                    
+                    single_trial_data.target{t}.(this_var).dim_red.st_scores(:,:,r) = ...
+                        emg_data.scores(single_trial_data.target{t}.bin_indx_p_trial(:,r),:);
+            end
+        end
+
+        % all concatenated trials
+        switch this_var
+            case 'neural_data'
+                single_trial_data.target{t}.(this_var).dim_red.scores = ...
+                    neural_data.scores(single_trial_data.target{t}.bin_indx,:); 
+%         single_trial_data.target{t}.(this_var).dim_red.t = ...
+%             (this_var).t(single_trial_data.target{t}.bin_indx);
+            case 'emg_data'
+                single_trial_data.target{t}.(this_var).dim_red.scores = ...
+                    emg_data.scores(single_trial_data.target{t}.bin_indx,:); 
+        end
+
+        % add mean and SD scores
+        single_trial_data.target{t}.(this_var).dim_red.st_scores_mn = ...
+            mean(single_trial_data.target{t}.(this_var).dim_red.st_scores,3);
+        single_trial_data.target{t}.(this_var).dim_red.st_scores_sd = ...
+            std(single_trial_data.target{t}.(this_var).dim_red.st_scores,0,3);
+    end
+
     % -----------------------------------
     % 2) fill last field of single_trial_data, which are all the
     % concatenated trials
-    
+
     % init vectors to concatenate data
-    aux_st_scores       = single_trial_data.target{1}.neural_data.dim_red.st_scores;
-    aux_scores          = single_trial_data.target{1}.neural_data.dim_red.scores;
-%     aux_t               = single_trial_data.target{1}.neural_data.dim_red.t;
-    
-    aux_st_scores_mn    = single_trial_data.target{1}.neural_data.dim_red.st_scores_mn;
-    aux_st_scores_sd    = single_trial_data.target{1}.neural_data.dim_red.st_scores_sd;
-    
+    aux_st_scores       = single_trial_data.target{1}.(this_var).dim_red.st_scores;
+    aux_scores          = single_trial_data.target{1}.(this_var).dim_red.scores;
+%     aux_t               = single_trial_data.target{1}.(this_var).dim_red.t;
+
+    aux_st_scores_mn    = single_trial_data.target{1}.(this_var).dim_red.st_scores_mn;
+    aux_st_scores_sd    = single_trial_data.target{1}.(this_var).dim_red.st_scores_sd;
+
     % add info for all the concatenated targets, as well as the scores
-    single_trial_data.target{end}.neural_data.dim_red.method = ...
-        neural_data.method;
-    single_trial_data.target{end}.neural_data.dim_red.w = ...
-        neural_data.w;
-    
+    switch this_var
+        case 'neural_data'
+            single_trial_data.target{end}.(this_var).dim_red.method = ...
+                neural_data.method;
+            single_trial_data.target{end}.(this_var).dim_red.w = ...
+                neural_data.w;
+        case 'emg_data'
+            single_trial_data.target{end}.(this_var).dim_red.method = ...
+                emg_data.method;
+            single_trial_data.target{end}.(this_var).dim_red.w = ...
+                emg_data.w;
+    end
+
     % concatenate scores & time
     for t = 2:nbr_targets
-        aux_st_scores  	= cat(3,aux_st_scores,single_trial_data.target{t}.neural_data.dim_red.st_scores);
-        aux_scores      = cat(1,aux_scores,single_trial_data.target{t}.neural_data.dim_red.scores);
-%         aux_t           = cat(1,aux_t,single_trial_data.target{t}.neural_data.dim_red.t);
-        
-        aux_st_scores_mn = cat(1,aux_st_scores_mn,single_trial_data.target{t}.neural_data.dim_red.st_scores_mn);
-        aux_st_scores_sd = cat(1,aux_st_scores_sd,single_trial_data.target{t}.neural_data.dim_red.st_scores_sd);
+        aux_st_scores  	= cat(3,aux_st_scores,single_trial_data.target{t}.(this_var).dim_red.st_scores);
+        aux_scores      = cat(1,aux_scores,single_trial_data.target{t}.(this_var).dim_red.scores);
+%         aux_t           = cat(1,aux_t,single_trial_data.target{t}.(this_var).dim_red.t);
+
+        aux_st_scores_mn = cat(1,aux_st_scores_mn,single_trial_data.target{t}.(this_var).dim_red.st_scores_mn);
+        aux_st_scores_sd = cat(1,aux_st_scores_sd,single_trial_data.target{t}.(this_var).dim_red.st_scores_sd);
     end
-%     single_trial_data.target{end}.neural_data.dim_red.t = aux_t;
-    single_trial_data.target{end}.neural_data.dim_red.scores = aux_scores;
-    
-    single_trial_data.target{end}.neural_data.dim_red.eigen = ...
-        neural_data.eigen;
-    single_trial_data.target{end}.neural_data.dim_red.chs = ...
-        neural_data.chs;    
-    
-    single_trial_data.target{end}.neural_data.dim_red.st_scores = ...
+%     single_trial_data.target{end}.(this_var).dim_red.t = aux_t;
+    single_trial_data.target{end}.(this_var).dim_red.scores = aux_scores;
+
+    switch this_var
+        case 'neural_data'
+            single_trial_data.target{end}.(this_var).dim_red.eigen = ...
+                neural_data.eigen;
+            single_trial_data.target{end}.(this_var).dim_red.chs = ...
+                neural_data.chs;    
+        case 'emg_data'
+            if isfield(emg_data,'eigen')
+                single_trial_data.target{end}.(this_var).dim_red.eigen = ...
+                    emg_data.eigen;
+            end
+            single_trial_data.target{end}.(this_var).dim_red.chs = ...
+                emg_data.chs;    
+    end
+
+    single_trial_data.target{end}.(this_var).dim_red.st_scores = ...
         aux_st_scores;
-    single_trial_data.target{end}.neural_data.dim_red.st_scores_mn = ...
+    single_trial_data.target{end}.(this_var).dim_red.st_scores_mn = ...
         aux_st_scores_mn;
-    single_trial_data.target{end}.neural_data.dim_red.st_scores_sd = ...
+    single_trial_data.target{end}.(this_var).dim_red.st_scores_sd = ...
         aux_st_scores_sd;
-end
-
-
-% -------------------------------------------------------------------------
-% add emg data
-if emg_data_yn
-    error ('adding dimensionally-reduced EMG not implemented yet')
 end
