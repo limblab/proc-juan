5f93dfbb9ea6775d021b7fa73f46518766fc2b94
jotagalabe <jotagalabe@gmail.com>
2016-09-10 21:04:05 -0500

    comp_hyperplanes_fcn_dim_finding_closest.m: added the possibilty of passing matrices of eigenvectors rather than a cell. Also included <<reverse search>> although I have to check that it is compatible with other fcns

--- dim_reduction/comp_hyperplanes_fcn_dim_finding_closest.m
+++ dim_reduction/comp_hyperplanes_fcn_dim_finding_closest.m
@@ -43,6 +43,28 @@ end
 % -------------------------------------------------------------------------
 % find closest eigenvector for each dimension
 % Do until n = N - 1
-[angles, dim_min_angle]         = find_closest_hyperplane( eigenv_1, eigenv_2, ...
-                                dims_hyper_in_1 );
-                                
+[~, dim_min_angle]              = find_closest_hyperplane( eigenv_1, eigenv_2, ...
+                                    dims_hyper_in_1 );
+
+% [~, dim_min_angle_rev]          = find_closest_hyperplane( eigenv_2, eigenv_1, ...
+%                                     dims_hyper_in_1 );
+
+% create cell with eigenvectors, for compatibility
+eigenv_cell{1}.w                = eigenv_1;
+eigenv_cell{2}.w                = eigenv_2;
+eigenv_order                    = [(1:length(dim_min_angle))', dim_min_angle'];
+angles                          = zeros(1,length(dim_min_angle));
+
+% eigenv_cell_rev{1}.w            = eigenv_2;
+% eigenv_cell_rev{2}.w            = eigenv_1;
+% eigenv_order_rev                = [(1:length(dim_min_angle_rev))', dim_min_angle'];
+% angles_rev                      = zeros(1,length(dim_min_angle));
+
+% do for all hyperplane dimensions
+for i = 1:length(dims_hyper_in_1)
+    [aux_angle, ~]              = summary_angle_btw_pcs( eigenv_cell, eigenv_order(dims_hyper_in_1(1:i),:), [], false );
+%     [aux_angle_rev,~]           = summary_angle_btw_pcs( eigenv_cell_rev, eigenv_order_rev(dims_hyper_in_1(1:i),:), [], false );
+    
+    angles(i)                   = aux_angle(1,2);
+%     angles_rev(i)               = aux_angle_rev(1,2);
+end
