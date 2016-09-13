d6f89faa92b0ee417cc041fe8772aff101938e93
jotagalabe <jotagalabe@gmail.com>
2016-09-13 10:33:44 -0500

    batch_compare_manifold_projs_defaults.m: added option to plot p session + comments

--- dim_reduction/_aux & wrapper functions/batch_compare_manifold_projs_defaults.m
+++ dim_reduction/_aux & wrapper functions/batch_compare_manifold_projs_defaults.m
@@ -3,6 +3,26 @@
 %
 %   function params = batch_compare_manifold_projs_defaults( varargin )
 %
+%
+% 'params' is a struct with fields:
+%
+% Field                 : [default]
+%   target              : ['all_conc'] target number
+%   dim_manifold        : [20] number of dimensions of the neural manifold
+%   P_thr               : [0.001] P value for assessing the significance of
+%                           the canonical correlations using Matlab's
+%                           built-in test 
+%   do_bootstrap        : [true] do bootstrapping to assess the
+%                           significance of the canonical correlations     
+%   prctile_bootstrap   : [99] the percentile CC that will be chosen as
+%                           significance threshold
+%   time_win            : [see code] time window that wil be used for the
+%                           analysis (t_init, t_end) per session
+%   plot_p_session      : [false] whether to plot one figure per session or
+%                           not, besides the summary plots
+%
+%
+
 
 function params = batch_compare_manifold_projs_defaults( varargin )
 
@@ -16,7 +36,8 @@ params_defaults     = struct( ...
                         'prctile_bootstrap',99, ...
                         'time_win',         [0.5 1.1; 0.3 0.9; 0.4 1;
                                             0 0.6; 0 0.6; 0 0.6; 
-                                            0.3 0.9; 0.2 0.8] );
+                                            0.3 0.9; 0.2 0.8], ...
+                        'plot_p_session',   false );
 
 
 % read input params, if passed
