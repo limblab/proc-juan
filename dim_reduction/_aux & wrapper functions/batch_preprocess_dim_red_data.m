6bdb8e6c7347402069594a23bb2d2e42dfdb4637
jotagalabe <jotagalabe@gmail.com>
2016-09-13 10:40:01 -0500

    batch_preprocess_dim_red_data.m: added the possibility of extracting muscle synergies across all tasks in a session, + saving the datasets optionally

--- dim_reduction/_aux & wrapper functions/batch_preprocess_dim_red_data.m
+++ dim_reduction/_aux & wrapper functions/batch_preprocess_dim_red_data.m
@@ -26,6 +26,10 @@
 function datasets = batch_preprocess_dim_red_data( varargin )
 
 
+% check that the parallel pool is running, otherwise start it
+gcp;
+
+
 % -------------------------------------------------------------------------
 % read input params
 
@@ -93,15 +97,20 @@ for i = 1:numel(files_indx)
         % decide what data to do PCA on --the continous data or the
         % trial-related part of the data only
         switch params.data_pca
+            
             case 'all'
                 datasets{i}.dim_red_FR{ii} = dim_reduction( datasets{i}.smoothed_FR{ii}, ...
                             params.dim_red_method, discard_neurons );
+                if params.dim_red_emg ~= 'none'
+                    warning('Dim reduction of the EMG not implemented for continuous data');
+                end
+                
             case 'trial-related'
                 
                 % ----------------------------------
                 % 1) split the data in single trials
                 % -- note that get-single_trial_data will already exclude
-                % the unwanted neural and EMG
+                % the unwanted neural and EMG channels
                 stdata          = get_single_trial_data( datasets{i}.binned_data{ii}, ...
                             labels(ii), neural_chs, chosen_emgs, params.norm_trial_data, ...
                             params.w_i, params.w_f );
@@ -126,18 +135,36 @@ for i = 1:numel(files_indx)
                     case 'none'
                         % do nothing
                     otherwise
-                        error('dim red EMG not implemented yet!');
+                        % do dim reduction if haven't specified to do it
+                        % across tasks
+                        if ~params.dim_red_emg_across_taks
+                            % get concatenated EMG data
+                            emg =[stdata.target{end}.conc_t', ...
+                                    stdata.target{end}.emg_data.conc_emg];
+                            % do PCA/NMF of the EMGs
+                            datasets{i}.dim_red_emg{ii} = dim_reduction_muscles( ...
+                                emg, params.dim_red_emg, [], [], params.emg_factors );
+                        end
                 end
                 % ----------------------------------
                 % 4) Rearrange results
                 
                 % add dimensionally reduced data to single trial data
                 % struct
-                if ~exist('dim_red_emg','var')
+                if ~isfield(datasets{i},'dim_red_emg')
+                    % add the dim_red_FR (neurons) only
                     stdata      = add_dim_red_data_to_single_trial_data( ...
                             stdata, datasets{i}.dim_red_FR{ii} ); 
+                elseif ~params.dim_red_emg_across_taks
+                    % add the dim_red_FR & dim_red_emg
+                    stdata      = add_dim_red_data_to_single_trial_data( ...
+                            stdata, datasets{i}.dim_red_FR{ii}, ...
+                            datasets{i}.dim_red_emg{ii} ); 
                 else
-                    error('to be implemented!');
+                    % only add dim_red_FR, if want to do PCA/NMF of the
+                    % EMGs for all the tasks
+                    stdata      = add_dim_red_data_to_single_trial_data( ...
+                            stdata, datasets{i}.dim_red_FR{ii} ); 
                 end
                 
                 % add dim_red data to single trial data struct
@@ -153,6 +180,15 @@ for i = 1:numel(files_indx)
         datasets{i}.smoothed_FR{ii} = sfr;
     end
     
+    
+    % do dim reduction of the EMGs across tasks, if specified
+    if ~strcmp(params.dim_red_emg,'none') && params.dim_red_emg_across_taks
+        tmp_dtst    = add_dim_red_emg_pooled_across_tasks( datasets{i}, params );
+        datasets{i}   = tmp_dtst;
+    end
+    % dim_red_emg = dim_reduction_muscles_pooling_tasks( data_set, 'nmf', [], 4, 0 )
+    
+    
     % clean up
     clear cbdf bd sfr chosen_emgs neural_chs discard_neurons labels
     
@@ -161,5 +197,7 @@ end
 
 
 % save all the datasets
-save('all_manifold_datasets.mat','datasets','-v7.3');
-disp(['data saved in ' pwd filesep 'all_manifold_datasets.mat']);
+if params.save_data
+    save('all_manifold_datasets.mat','datasets','-v7.3');
+    disp(['data saved in ' pwd filesep 'all_manifold_datasets.mat']);
+end
