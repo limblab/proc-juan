a9f5fca4b8d1d1c7facacd9169789b3d9c7e97a9
jotagalabe <jotagalabe@gmail.com>
2016-09-14 00:38:47 -0500

    add_dim_red_data_to_single_trial_data.m: fixed a fatal error retrieving the single trials -working now

--- aux_BDF_bin_functions/add_dim_red_data_to_single_trial_data.m
+++ aux_BDF_bin_functions/add_dim_red_data_to_single_trial_data.m
@@ -25,6 +25,9 @@
 %   dim_red_neural structs, because it is measured w.r.t
 %   cropped_binned_data (rather than binned_data), which means that targets
 %   may start back to back  
+%   - ToDo: make the code more compact, there are many things that can be
+%   merged !!!!
+%
 %
 
 function single_trial_data = add_dim_red_data_to_single_trial_data( ...
@@ -82,6 +85,10 @@ for v = 1:length(indx_vars_to_add)
     nbr_targets         = length(single_trial_data.target) - 1;
     nbr_bins            = size(single_trial_data.target{1}.bin_indx_p_trial,1);
     
+    % get number of trials per task
+    nbr_trials_p_target = cellfun( @(x) size(x.bin_indx_p_trial,2), single_trial_data.target );
+    nbr_trials_p_target(end) = [];
+    offset_nbr_trials_p_target = [0, nbr_bins*cumsum(nbr_trials_p_target(1:end-1))];
 
     % -----------------------------------
     % 1) add scores (projections onto PCs) for each target
@@ -89,7 +96,7 @@ for v = 1:length(indx_vars_to_add)
     for t = 1:nbr_targets
 
         % individual trials
-        nbr_trials_this_tgt = size(single_trial_data.target{t}.bin_indx_p_trial,2);
+        nbr_trials_this_tgt = nbr_trials_p_target(t);
 
         switch this_var
             case 'neural_data'
@@ -100,41 +107,61 @@ for v = 1:length(indx_vars_to_add)
                     zeros(nbr_bins,size(emg_data.w,2),nbr_trials_this_tgt);
         end
         
+        % NOTE: these trials are no longer ordered according to the bin
+        % indexes in single_trial_data, because the dim red data is
+        % obtained from the concatenated FRs/EMGs in
+        % single_trial_data.target{end} (one after another) 
         for r = 1:nbr_trials_this_tgt
             switch this_var
                 case 'neural_data'
+                    % get index that define the beginning and the end of
+                    % the trial
+                    start_indx      = offset_nbr_trials_p_target(t) + 1 + (r-1)*nbr_bins;
+                    end_indx        = offset_nbr_trials_p_target(t) + r*nbr_bins;
+                    
                     single_trial_data.target{t}.(this_var).dim_red.st_scores(:,:,r) = ...
-                        neural_data.scores(single_trial_data.target{t}.bin_indx_p_trial(:,r),:);
+                        neural_data.scores(start_indx:end_indx,:);
+                    
                 case 'emg_data'
+                    start_indx      = offset_nbr_trials_p_target(t) + 1 + (r-1)*nbr_bins;
+                    end_indx        = offset_nbr_trials_p_target(t) + r*nbr_bins;
+                    
                     single_trial_data.target{t}.(this_var).dim_red.st_scores(:,:,r) = ...
-                        emg_data.scores(single_trial_data.target{t}.bin_indx_p_trial(:,r),:);
+                        emg_data.scores(start_indx:end_indx,:);
             end
         end
 
         % all concatenated trials
         switch this_var
             case 'neural_data'
+                start_indx          = offset_nbr_trials_p_target(t) + 1;
+                end_indx            = offset_nbr_trials_p_target(t) + nbr_trials_this_tgt*nbr_bins;
+                
                 single_trial_data.target{t}.(this_var).dim_red.scores = ...
-                    neural_data.scores(single_trial_data.target{t}.bin_indx,:); 
-%         single_trial_data.target{t}.(this_var).dim_red.t = ...
-%             (this_var).t(single_trial_data.target{t}.bin_indx);
+                    neural_data.scores(start_indx:end_indx,:); 
+
             case 'emg_data'
+                start_indx          = offset_nbr_trials_p_target(t) + 1;
+                end_indx            = offset_nbr_trials_p_target(t) + nbr_trials_this_tgt*nbr_bins;
+                
                 single_trial_data.target{t}.(this_var).dim_red.scores = ...
-                    emg_data.scores(single_trial_data.target{t}.bin_indx,:); 
+                    emg_data.scores(start_indx:end_indx,:); 
         end
 
         % add mean and SD scores
+        
         single_trial_data.target{t}.(this_var).dim_red.st_scores_mn = ...
             mean(single_trial_data.target{t}.(this_var).dim_red.st_scores,3);
         single_trial_data.target{t}.(this_var).dim_red.st_scores_sd = ...
             std(single_trial_data.target{t}.(this_var).dim_red.st_scores,0,3);
     end
 
+    
     % -----------------------------------
     % 2) fill last field of single_trial_data, which are all the
     % concatenated trials
 
-    % init vectors to concatenate data
+    % init vectors to concatenate data with the values from the 1st target
     aux_st_scores       = single_trial_data.target{1}.(this_var).dim_red.st_scores;
     aux_scores          = single_trial_data.target{1}.(this_var).dim_red.scores;
 %     aux_t               = single_trial_data.target{1}.(this_var).dim_red.t;
@@ -142,7 +169,8 @@ for v = 1:length(indx_vars_to_add)
     aux_st_scores_mn    = single_trial_data.target{1}.(this_var).dim_red.st_scores_mn;
     aux_st_scores_sd    = single_trial_data.target{1}.(this_var).dim_red.st_scores_sd;
 
-    % add info for all the concatenated targets, as well as the scores
+    % add dimensionality reduciton results (PC matrices, what method was
+    % used, etc)
     switch this_var
         case 'neural_data'
             single_trial_data.target{end}.(this_var).dim_red.method = ...
@@ -156,18 +184,18 @@ for v = 1:length(indx_vars_to_add)
                 emg_data.w;
     end
 
-    % concatenate scores & time
+    % concatenate scores, single trials & mean + SD
     for t = 2:nbr_targets
-        aux_st_scores  	= cat(3,aux_st_scores,single_trial_data.target{t}.(this_var).dim_red.st_scores);
-        aux_scores      = cat(1,aux_scores,single_trial_data.target{t}.(this_var).dim_red.scores);
+        aux_st_scores  	= cat( 3, aux_st_scores, single_trial_data.target{t}.(this_var).dim_red.st_scores );
+        aux_scores      = cat( 1, aux_scores, single_trial_data.target{t}.(this_var).dim_red.scores );
 %         aux_t           = cat(1,aux_t,single_trial_data.target{t}.(this_var).dim_red.t);
 
-        aux_st_scores_mn = cat(1,aux_st_scores_mn,single_trial_data.target{t}.(this_var).dim_red.st_scores_mn);
-        aux_st_scores_sd = cat(1,aux_st_scores_sd,single_trial_data.target{t}.(this_var).dim_red.st_scores_sd);
+        aux_st_scores_mn = cat( 1, aux_st_scores_mn, single_trial_data.target{t}.(this_var).dim_red.st_scores_mn );
+        aux_st_scores_sd = cat( 1, aux_st_scores_sd, single_trial_data.target{t}.(this_var).dim_red.st_scores_sd );
     end
-%     single_trial_data.target{end}.(this_var).dim_red.t = aux_t;
     single_trial_data.target{end}.(this_var).dim_red.scores = aux_scores;
 
+    % add eigenvalue distribution (if PCA) and used channels
     switch this_var
         case 'neural_data'
             single_trial_data.target{end}.(this_var).dim_red.eigen = ...
@@ -183,6 +211,7 @@ for v = 1:length(indx_vars_to_add)
                 emg_data.chs;    
     end
 
+    % & save the latent variables / scores
     single_trial_data.target{end}.(this_var).dim_red.st_scores = ...
         aux_st_scores;
     single_trial_data.target{end}.(this_var).dim_red.st_scores_mn = ...
