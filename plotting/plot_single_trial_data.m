e7ac6df349bd04d4f0f901f4023cef418b5fb631
jotagalabe <jotagalabe@gmail.com>
2016-09-24 18:24:46 -0500

    plot_single_trial_data.m: minor changes

--- plotting/plot_single_trial_data.m
+++ plotting/plot_single_trial_data.m
@@ -11,8 +11,12 @@
 %   param               : what neurons/emgs/pcs/synergies/pos signals/vel
 %                           signals/force signals to plot
 %   (target)            : ['each'] target number: 'each') one plot per
-%                           target; scalar 1:n) target number; 'all') for
-%                           all concatenated trials
+%                           target, showing the data from all the trials,
+%                           as well as the mean (+/-SD); scalar 1:n) target
+%                           number, same format as 'each'; 'all') for all
+%                           trials in the same plot
+%   (superimpose)       : [false] plot the data from each target in the
+%                           same subplot
 %
 %
 
@@ -23,8 +27,13 @@ function plot_single_trial_data( single_trial_data, var, params, varargin )
 % read inputs
 if nargin == 4
     target              = varargin{1};
+    superimpose         = false;
+elseif nargin == 5
+    target              = varargin{1};
+    superimpose         = varargin{2};
 else
     target              = 'each';
+    superimpose         = false;
 end
 
 
@@ -51,7 +60,7 @@ nbr_vars                = length(params);
 % nbr of targets to plot per task
 if isnumeric(target)
     
-    % if passed the trial number
+    % if passed the target number
     targets_to_plot     = cell(1,nbr_bdfs);
     for b = 1:nbr_bdfs
         targets_to_plot{b} = target;
@@ -137,61 +146,70 @@ end
 % -------------------------------------------------------------------------
 % Plot
     
-
 % get list of targets --can be different across tasks, and whether we want
 % to plot all concatenated targets or just one of them
-switch target
-    case 'all'
-        possible_tgts = cellfun( @(x) length(x.target), single_trial_data, 'UniformOutput', false );
-        warning('this calculates the mean across all targets, rather than plotting one, the another, etc');
-    otherwise
-        possible_tgts = cellfun( @(x) 1:length(x.target)-1, single_trial_data, 'UniformOutput', false );
+if ischar(target)
+    switch target
+        case 'all'
+            possible_tgts = cellfun( @(x) length(x.target), single_trial_data, 'UniformOutput', false );
+            warning('this calculates the mean across all targets, rather than plotting one, the another, etc');
+        otherwise
+            possible_tgts = cellfun( @(x) 1:length(x.target)-1, single_trial_data, 'UniformOutput', false );
+    end
+elseif isnumeric(target)
+    possible_tgts = cellfun( @(x) 1:length(x.target)-1, single_trial_data, 'UniformOutput', false );
 end
 
+
 % do a plot per target
-for t = 1:nbr_targets_to_plot
+if ~superimpose 
+    for t = 1:nbr_targets_to_plot
 
-    % independent plot per target
-    % figure('units','normalized','outerposition',[0 0 1 1])
-    figure
-    
-    % overlay plot per BDF (task)
-    for b = 1:nbr_bdfs
-        % independent plot in this figure for each neuron/force/emg...
-        for v = 1:nbr_vars
+        % independent plot per target
+        % figure('units','normalized','outerposition',[0 0 1 1])
+        figure
 
-            if ismember(targets_to_plot{b}(t),possible_tgts{b})
-                this_tgt        = targets_to_plot{b}(t);
-            
-                % the corresponding subplot
-                subplot(rows_plot,cols_plot,v), hold on
-            
-                % get mean +/-SD of the variable 
-                %
-                % check if var name is a 3-by-1 cell (for the neural pcs or
-                % muscle synergies) or a 2-by-1 cell (for the rest) 
-                if length(var_type) == 2
-                    aux_data    = squeeze(single_trial_data{b}.target{this_tgt}.(var_type{1}).(var_type{2})...
-                                    (:,params(v),:));
-                else
-                    aux_data    = squeeze(single_trial_data{b}.target{this_tgt}.(var_type{1}).(var_type{2}).(var_type{3})...
-                                    (:,params(v),:));
+        % overlay plot per BDF (task)
+        for b = 1:nbr_bdfs
+            % independent plot in this figure for each neuron/force/emg...
+            for v = 1:nbr_vars
+
+                if ismember(targets_to_plot{b}(t),possible_tgts{b})
+                    this_tgt        = targets_to_plot{b}(t);
+
+                    % the corresponding subplot
+                    subplot(rows_plot,cols_plot,v), hold on
+
+                    % get mean +/-SD of the variable 
+                    %
+                    % check if var name is a 3-by-1 cell (for the neural pcs or
+                    % muscle synergies) or a 2-by-1 cell (for the rest) 
+                    if length(var_type) == 2
+                        aux_data    = squeeze(single_trial_data{b}.target{this_tgt}.(var_type{1}).(var_type{2})...
+                                        (:,params(v),:));
+                    else
+                        aux_data    = squeeze(single_trial_data{b}.target{this_tgt}.(var_type{1}).(var_type{2}).(var_type{3})...
+                                        (:,params(v),:));
+                    end
+                    aux_mean        = mean(aux_data,2);
+                    aux_sd          = std(aux_data,0,2);
+                    color_indx      = v+(b-1)*nbr_vars;
+                    if nbr_bdfs == 1, plot( t_axis{b}, aux_data, 'color',[.65 .65 .65]); end
+                    plot( t_axis{b}, aux_mean, 'color',colors_plot(color_indx,:),'linewidth',3);
+                    plot( t_axis{b}, aux_mean+aux_sd, ':','color',colors_plot(color_indx,:),'linewidth',3);
+                    plot( t_axis{b}, aux_mean-aux_sd, ':','color',colors_plot(color_indx,:),'linewidth',3);
+                    set(gca,'TickDir','out'), set(gca,'FontSize',14);
                 end
-                aux_mean        = mean(aux_data,2);
-                aux_sd          = std(aux_data,0,2);
-                color_indx      = v+(b-1)*nbr_vars;
-                if nbr_bdfs == 1, plot( t_axis{b}, aux_data, 'color',[.65 .65 .65]); end
-                plot( t_axis{b}, aux_mean, 'color',colors_plot(color_indx,:),'linewidth',3);
-                plot( t_axis{b}, aux_mean+aux_sd, ':','color',colors_plot(color_indx,:),'linewidth',3);
-                plot( t_axis{b}, aux_mean-aux_sd, ':','color',colors_plot(color_indx,:),'linewidth',3);
-                set(gca,'TickDir','out'), set(gca,'FontSize',14);
+
+                % add labels
+                if v > cols_plot*(rows_plot-1)
+                    xlabel('time (s)')
+                end
+                ylabel([var ' ' num2str(v)],'Interpreter','none');
             end
-        
-            % add labels
-            if v > cols_plot*(rows_plot-1)
-                xlabel('time (s)')
-            end
-            ylabel([var ' ' num2str(v)],'Interpreter','none');
         end
     end
+%superimpose the data from each target
+else
+    
 end
