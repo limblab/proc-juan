3193f9106f885899d75c6a4839110979f17fb1fb
jotagalabe <jotagalabe@gmail.com>
2016-11-04 11:51:28 -0500

    plot_pca_weights: added equal axes for all plots

--- plotting/plot_pca_weights.m
+++ plotting/plot_pca_weights.m
@@ -28,11 +28,18 @@ nbr_chs             = length(dim_red_FR{1}.eigen);
 nbr_rows            = floor(sqrt(nbr_bdfs));
 nbr_cols            = ceil(nbr_bdfs/nbr_rows);
 
+% make C axis a little bit "wider" than what we need and use the same for
+% all panels
+c_axis_max          = ceil( max( cellfun( @(x) max(max(abs(x.w))), dim_red_FR ) )*10 )/ 10;
+x_ticks             = 10:10:(floor(nbr_chs/10)*10);
+
 % plot
 figure
 for i = 1:nbr_bdfs
     subplot(nbr_rows,nbr_cols,i)
     imagesc(1:nbr_chs,1:nbr_chs,abs(dim_red_FR{i}.w)),colorbar
+    caxis([0 c_axis_max])
     set(gca,'TickDir','out','FontSize',14)
+    set(gca,'XTick',x_ticks)
     title(labels(i)),xlabel('principal component'),ylabel('neural channel')
 end
