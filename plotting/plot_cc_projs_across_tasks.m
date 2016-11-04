a961375d91a5e58dee558c398aa5d5903fd10158
jotagalabe <jotagalabe@gmail.com>
2016-11-04 11:56:12 -0500

    plot_cc_projs_across_tasks: minor changes

--- plotting/plot_cc_projs_across_tasks.m
+++ plotting/plot_cc_projs_across_tasks.m
@@ -19,7 +19,7 @@ end
 
 
 % create time vector
-t                   = 0:bin_size:bin_size*(length(proj_results{session}.can_corrs.lin_transform(1).U)-1);
+t                   = 0:bin_size:bin_size*(length(proj_results.data{session}.can_corrs.lin_transform(1).U)-1);
 
 % define time for vertical lines to split trials
 trial_length        = (params.time_win(session,2)-params.time_win(session,1))/bin_size+1;
@@ -28,20 +28,20 @@ vline_samples       = (0:nbr_trials)*trial_length;
 vline_samples(1)    = [];
 
 % get axis scale
-max_scale           = max(max([abs(proj_results{session}.can_corrs.lin_transform(pair).U(:,1:nbr_projs)), ...
-                        abs(proj_results{session}.can_corrs.lin_transform(pair).V(:,1:nbr_projs))]));
+max_scale           = max(max([abs(proj_results.data{session}.can_corrs.lin_transform(pair).U(:,1:nbr_projs)), ...
+                        abs(proj_results.data{session}.can_corrs.lin_transform(pair).V(:,1:nbr_projs))]));
 max_scale           = ceil(max_scale);
 
 figure
 for i = 1:nbr_projs
     subplot(nbr_projs,1,i), hold on
-    plot( t, proj_results{session}.can_corrs.lin_transform(pair).U(:,i),'b','linewidth',2)
-    plot( t, proj_results{session}.can_corrs.lin_transform(pair).V(:,i),'c','linewidth',2)
+    plot( t, proj_results.data{session}.can_corrs.lin_transform(pair).U(:,i),'b','linewidth',2)
+    plot( t, proj_results.data{session}.can_corrs.lin_transform(pair).V(:,i),'c','linewidth',2)
     for l = 1:length(vline_samples)
-        plot([t(vline_samples(l)) t(vline_samples(l))],[-max_scale max_scale],'w','linewidth',4)
+        plot([t(round(vline_samples(l))) t(round(vline_samples(l)))],[-max_scale max_scale],'w','linewidth',4)
     end
     
-    legend(['r=' num2str(proj_results{session}.can_corrs.cc(pair,i))]), legend boxoff
+    legend(['r=' num2str(proj_results.data{session}.can_corrs.cc(pair,i))]), legend boxoff
     set(gca,'TickDir','out','FontSize',14)
     xlim([15 30]), ylim([-max_scale, max_scale])
     if i == nbr_projs
