6cd02283b5e91c41a91d002a7d86d8c9ec41f4b3
jotagalabe <jotagalabe@gmail.com>
2018-03-27 22:02:43 +0200

    vaf_PAs: fixed calculation total variance & added some plots

--- paper_cross-task_manifold_review/vaf_PAs.m
+++ paper_cross-task_manifold_review/vaf_PAs.m
@@ -20,6 +20,8 @@ mani_dims = 12;
 % --don't really know why, to compare them?
 all_vars_A = [];
 all_vars_B = [];
+norm_all_vars_A = [];
+norm_all_vars_B = [];
 wrist_var = [];
 
 
@@ -72,14 +74,26 @@ for d = 1:length(ds_to_use)
         modesb = frb*Bpa;
 
         % compute their normalized variance
-        vara = var(modesa,1)/sum(var(modesa,1));
-        varb = var(modesb,1)/sum(var(modesb,1));
+        vara = var(modesa,1);
+        varb = var(modesb,1);
+        
+        tot_vara = sum(var(datasets{t_ds}.dim_red_FR{t1}.scores));
+        tot_varb = sum(var(datasets{t_ds}.dim_red_FR{t2}.scores));
+        
+%         norm_vara = var(modesa,1)/sum(var(modesa,1));
+%         norm_varb = var(modesb,1)/sum(var(modesb,1));
+        norm_vara = var(modesa,1)/tot_vara;
+        norm_varb = var(modesb,1)/tot_varb;
+
 
         % -----------------------------------------------------------------
         % Store all variances
         
         all_vars_A = [all_vars_A; vara];
-        all_vars_B = [all_vars_B; varb]; %#ok<*AGROW>
+        all_vars_B = [all_vars_B; varb];
+        
+        norm_all_vars_A = [norm_all_vars_A; norm_vara];
+        norm_all_vars_B = [norm_all_vars_B; norm_varb]; %#ok<*AGROW>
         
         if ismember(t_ds,ds_wrist)
             wrist_var = [wrist_var; 1];
@@ -90,33 +104,73 @@ for d = 1:length(ds_to_use)
 end
 
 
-% -------------------------------------------------------------------------
-% Summary plots
+%%-------------------------------------------------------------------------
+% Summary stats
 
 
 % combined both VAFs (all_vars_A and all_vars_B) since it doesn't really
 % change
 
 all_vars = [all_vars_A; all_vars_B];
+norm_all_vars = [norm_all_vars_A; norm_all_vars_B];
+
+cumsum_norm_all_vars = cumsum(norm_all_vars,2);
 
 mn_all_vars = mean(all_vars,1);
 sd_all_vars = std(all_vars,0,1);
 
+mn_norm_all_vars = mean(norm_all_vars,1);
+sd_norm_all_vars = std(norm_all_vars,0,1);
+
+mn_cumsum_norm_all_vars = mean(cumsum_norm_all_vars,1);
+sd_cumsum_norm_all_vars = std(cumsum_norm_all_vars,0,1);
+
+
+%%-------------------------------------------------------------------------
+% Summary plots
+
+
+% % PLOT "RAW" VARIANCE
+% figure, hold on
+% plot(100*all_vars','color',[.6 .6 .6])
+% plot(100*mn_all_vars,'k','linewidth',2)
+% plot(100*(mn_all_vars+sd_all_vars),'-.k','linewidth',2)
+% plot(100*(mn_all_vars-sd_all_vars),'-.k','linewidth',2)
+% % errorbar(100*mn_all_vars,100*sd_all_vars,'.k','linestyle','none','linewidth',1.5,'markersize',30)
+% set(gca,'TickDir','out','FontSize',14), box off
+% xlabel('Principal-angle projected neural mode')
+% ylabel('Neural variance explained')
+
+
+% "NORMALIZED" VARIANCE
 figure, hold on
-plot(100*all_vars','color',[.6 .6 .6])
-plot(100*mn_all_vars,'k','linewidth',2)
-plot(100*(mn_all_vars+sd_all_vars),'-.k','linewidth',2)
-plot(100*(mn_all_vars-sd_all_vars),'-.k','linewidth',2)
+plot(100*norm_all_vars','color',[.6 .6 .6])
+plot(100*mn_norm_all_vars,'k','linewidth',2)
+plot(100*(mn_norm_all_vars+sd_norm_all_vars),'-.k','linewidth',2)
+plot(100*(mn_norm_all_vars-sd_norm_all_vars),'-.k','linewidth',2)
 % errorbar(100*mn_all_vars,100*sd_all_vars,'.k','linestyle','none','linewidth',1.5,'markersize',30)
 set(gca,'TickDir','out','FontSize',14), box off
 xlabel('Principal-angle projected neural mode')
 ylabel('Neural variance explained (%)')
 
 
+% CUMSUM VARIANCE
+figure, hold on
+plot(100*cumsum_norm_all_vars','color',[.6 .6 .6])
+plot(100*mn_cumsum_norm_all_vars,'k','linewidth',2)
+plot(100*(mn_cumsum_norm_all_vars+sd_cumsum_norm_all_vars),'-.k','linewidth',2)
+plot(100*(mn_cumsum_norm_all_vars-sd_cumsum_norm_all_vars),'-.k','linewidth',2)
+% errorbar(100*mn_all_vars,100*sd_all_vars,'.k','linestyle','none','linewidth',1.5,'markersize',30)
+set(gca,'TickDir','out','FontSize',14), box off
+xlabel('Principal-angle projected neural mode')
+ylabel('Cumulative neural variance explained (%)')
+
+
+
 % % separating by tasks
 % figure, hold on
-% plot(100*all_vars(wrist_var==1,:)','color',[.6 .6 .6])
-% plot(100*all_vars(wrist_var==0,:)','color',[1 .6 0])
+% plot(100*norm_all_vars(wrist_var==1,:)','color',[.6 .6 .6])
+% plot(100*norm_all_vars(wrist_var==0,:)','color',[1 .6 0])
 % % errorbar(100*mn_all_vars,100*sd_all_vars,'.k','linestyle','none','linewidth',1.5,'markersize',30)
 % set(gca,'TickDir','out','FontSize',14), box off
 % xlabel('Neural mode (after computing principal angles)')
