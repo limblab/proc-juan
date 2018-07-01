708d2b4168652176d1fd959b97eaabb48ee9d6aa
jotagalabe <jotagalabe@gmail.com>
2018-07-01 23:54:35 +0200

    Suppl_Fig_9b__EMG_var_expl_per_monkey_and_marginalization: added individual datapoints to bar plot

---
+++ paper_cross-task_manifold_figs/Suppl_Fig_9b__EMG_var_expl_per_monkey_and_marginalization.m
@@ -0,0 +1,77 @@
+%
+% Plot EMG variance per marginalization, pooled over all sessions for
+% each monkey separately
+%
+
+
+% datasets per monkey
+s_p_monkey      = {1:3,4:6}; % C and J
+
+% paper colors
+marg_cols       = [.4 .6 .5; .7 .1 .1; .5 .65 .9; 1 .6 .3];
+
+% color individual datapoints
+pt_cols         = [.75 .75 .75];
+
+% marg labels
+marg_labels     = {'task','target','time','task-target'};
+
+
+% create matrices with data
+for m = 1:length(s_p_monkey)
+    
+    % cell array expl_var per monkey (session x marg )
+    expl_var{m} = zeros(length(s_p_monkey{m}),length(marg_labels));
+    
+    for s = 1:length(s_p_monkey{m})
+        
+        expl_var{m}(s,:) = dPCA_emg_results{s_p_monkey{m}(s)}.expl_var.totalMarginalizedVar / ...
+                        dPCA_emg_results{s_p_monkey{m}(s)}.expl_var.totalVar * 100;
+    end
+end
+
+
+% do stats
+m_var_marg  = cell2mat(cellfun(@(x) mean(x), expl_var,'UniformOutput',false)');
+sd_var_marg = cell2mat(cellfun(@(x) std(x), expl_var,'UniformOutput',false)');
+
+
+% create matrices with all datapoints
+
+
+
+
+% change order of the data and the lab so marginalizations are shown as
+% task-independet and task dependent
+marg_swap   = [3 2 1 4];
+m_var_marg  = m_var_marg(:,marg_swap);
+sd_var_marg = sd_var_marg(:,marg_swap);
+marg_labels = marg_labels(marg_swap);
+marg_cols   = marg_cols(marg_swap,:);
+
+% plot, including individual datapoints
+figure, hold on
+for m = 1:length(marg_labels)
+    for k = length(s_p_monkey):-1:1
+        if k == 1 % for monkey 1 empty bars
+            errorbar( 3*m-1, m_var_marg(k,m), 0, sd_var_marg(k,m),'color',marg_cols(m,:),'marker','none','Linewidth',1.5);
+            tb(m) = bar( 3*m-1, m_var_marg(k,m) );
+            set(tb(m),'FaceColor',[1 1 1],'EdgeColor',marg_cols(m,:),'LineWidth',3)
+            plot(3*m-1+.25, expl_var{k}(:,marg_swap(m)), 'o', 'color', pt_cols, 'linestyle', 'none')
+        else % for monkey 2 filled bars
+            errorbar( 3*m-2, m_var_marg(k,m), 0, sd_var_marg(k,m),'color',marg_cols(m,:),'marker','none','Linewidth',1.5);
+            tb2(m) = bar( 3*m-2, m_var_marg(k,m) );
+            set(tb2(m),'FaceColor',marg_cols(m,:),'EdgeColor',marg_cols(m,:),'LineWidth',3)
+            plot(3*m-2+.25, expl_var{k}(:,marg_swap(m)), 'o', 'color', pt_cols, 'linestyle', 'none')
+        end
+    end
+end
+set(gca,'TickDir','out','FontSize',14), box off
+ylabel('EMG var. expl. (%)'), ylim([0 60])
+set(gca,'XTick',1.5:3:10.5,'XTickLabel',marg_labels,'XTickLabelRotation',45)
+legend([tb2(1) tb(1)],'J','C'),legend boxoff
+
+
+
+% Clear vars
+clearvars -except *_results *_params datasets manifold_dim
