40d0e11e9f9d067b6f1c566dffdc8c9885e05ef9
jotagalabe <jotagalabe@gmail.com>
2018-08-01 13:35:18 +0200

    main_stability_over_time: refined the code, fixed a couple minor things --running with all the monkeysgit add main_stability_over_time.m

--- stability_over_time/main_stability_over_time.m
+++ stability_over_time/main_stability_over_time.m
@@ -14,169 +14,18 @@ clear, close all
 % OPTIONS
 %
 
-
 % -------------------------------------------------------------------------
 % What data to use
 
-pars.monkey         = 'Chips'; % 'chewie2'; 'chewie'; 'mihili'; 'han'; 'chips'; 'jaco'
-pars.spiking_inputs = {'S1_spikes'}; % {'PMd_spikes'}; {'M1_spikes'}; {'S1_spikes'}
+pars.monkey         = 'jaco'; % 'chewie2'; 'chewie'; 'mihili'; 'han'; 'chips'; 'jaco'
+pars.spiking_inputs = {'M1_spikes'}; % {'PMd_spikes'}; {'M1_spikes'}; {'S1_spikes'}
 
 % Sesssions to discard if any
 pars.sessions_discard = []; %6:14; %[12 13 14];
 
 % -------------------------------------------------------------------------
-% Data preprocessing
-
-% "Unsort" the sorted neurons?
-pars.unsorted_yn                = true; 
-% Only use electrodes that have units in all the sessions (aonly for
-% unsorted_yn == true)
-% pars.only_common_elecs          = false; % NO LONGER AVAILABLE as an option; there are very few channels that are stable across all sessions
-
-% Gaussian kernel for smoothing
-pars.kernel_SD                  = 0.05;
-
-% "Downsampling rate": nbr of bins that will be combined
-pars.n_bins_downs               = 3;
-
-% Window start & end --if idx_end is empty: the duration of the shortest trial 
-% WATCH OUT -- THIS IS AFTER DOWNSAMPLING SO I NEED TO UPDATE IT BASED ON
-% N_BINS_DOWNS
-switch lower(pars.monkey)
-    case {'chips'} % S1
-        pars.mani_dims = 1:8; % Neural modes to use
-        pars.idx_start          = {'idx_go_cue', 0};
-        pars.idx_end            = {'idx_go_cue', 19};
-    case {'han'} % S1
-        pars.mani_dims = 1:8;  % Neural modes to use
-        pars.idx_start          = {'idx_go_cue', 0};
-        pars.idx_end            = {'idx_go_cue', 22};
-    case {'chewie','chewie2','mihili','mrt','jaco'}
-        switch pars.spiking_inputs{1}
-            case 'M1s_spikes'
-                pars.mani_dims = 1:10; % Neural modes to use
-%                 pars.idx_start  = {'idx_movement_on',-2}; % {'idx_movement_on',0}; % {'idx_go_cue',0}
-%                 pars.idx_end    = {'idx_movement_on',13}; % {'idx_movement_on',13}% {''}; % {'idx_go_cue',18}
-                pars.idx_start  = {'idx_movement_on',-4}; % {'idx_movement_on',0}; % {'idx_go_cue',0}
-                pars.idx_end    = {'idx_movement_on',13}; % {'idx_movement_on',13}% {''}; % {'idx_go_cue',18}
-            case 'PMd_spikes'
-                pars.mani_dims = 1:16; % Neural modes to use
-                pars.idx_start  = {'idx_movement_on',-15};
-                pars.idx_end    = {'idx_movement_on',0};
-        end
-end
-
-% -------------------------------------------------------------------------
-% To remove bad units
-pars.remove_bad_neurons                 = true; % For sorted neurons it always does it
-% Minimum FR per channel
-pars.bad_neuron_params.min_fr           = 0.1;
-
-% if strcmpi(pars.monkey,'chewie2'), pars.bad_neuron_params.min_fr = 0.5; end
-
-% Do shunt check
-pars.bad_neuron_params.shunt_check_yn   = false;
-
-% -------------------------------------------------------------------------
-% To remove bad trials --monkey was distracted and RT was too long
-
-% % done when data still are in 10 ms bins
-% pars.bad_trial_params_PMd               = struct( ...
-%                                         'ranges',{{ ...
-%                                         'idx_go_cue','idx_movement_on',[0 60]; ...
-%                                         'idx_target_on','idx_go_cue',[65 Inf]}});
-
-% % done when data still are in 10 ms bins
-% % SEE IF WE NEED TO CHECK THAT THE MONKEY HAD SOME REACTION TIME !!!
-% pars.bad_trial_params_M1                = struct( ...
-%                                         'ranges',{{ ...
-%                                         'idx_movement_on','idx_trial_end',[42 Inf]}});
-
-% % done when data still are in 10 ms bins
-% pars.bad_trial_params_S1                = struct( ...
-%                                         'ranges',{{ ...
-%                                         'idx_movement_on','idx_trial_end',[36 Inf]}});                                    
-                                    
-% -------------------------------------------------------------------------
-% Parameters to align latent activity
-pars.align_latent_params.xval_yn        = false;
-pars.align_latent_params.n_folds        = 6;
-
-pars.align_latent_params.method         = 'cca'; % 'cca' 'procrustes'
-
-pars.align_latent_params.n_shuff        = 100; % n shuffles for within day ceiling
-
-pars.align_latent_params.signals        = [pars.spiking_inputs{1}(1:find(pars.spiking_inputs{1}=='_')) 'pca'];
-pars.align_latent_params.mani_dims      = pars.mani_dims;
-
-pars.align_latent_params.save_fig       = true;
-
-pars.align_latent_params.top_lv_plot    = ceil(length(pars.mani_dims)/3);
-
-% -------------------------------------------------------------------------
-% Decoder settings -to predict behavior. In the current version, only done
-% for M1 and S1, since for PMd we classify target location basde on
-% preparatory activity
-
-% Inputs and Outputs
-pars.decoder_params.out                 = 'vel';
-pars.decoder_params.in                  = 'aligned_data'; % 'aligned_data'; 'unaligned_data'; 'spikes' (it always does spikes afterwards)
-
-% Bins for decoder history
-pars.decoder_params.hist_bins           = 3;
-
-% Lag of the kinematics we want to predict, only for S1 (because we predict
-% past kinematics)
-pars.decoder_params.lag_kin_S1          = 0; % 0.03; % (s)
-
-% Folds for multi-fold cross-validation
-pars.decoder_params.n_folds             = 6; 
-
-% A couple other slightly redundant definitions
-pars.decoder_params.manifold            = [pars.spiking_inputs{1}(1:end-7) '_pca'];
-pars.decoder_params.mani_dims           = pars.mani_dims;
-
-pars.decoder_params.unsort_chs_to_pred = true; % "unsort" channels to predict
-
-pars.decoder_params.save_fig            = true;
-
-% -------------------------------------------------------------------------
-% Classifier settings -to predict target location based on the preparatory
-% activity
-
-% Method
-pars.class_params.method                = 'Bayes'; % 'NN' 'Bayes'
-
-% Inputs and Outputs
-pars.class_params.out                   = 'target_direction';
-% will do spikes after this one, by default
-pars.class_params.in                    = 'aligned_data'; % 'aligned_data'; 'unaligned_data'; 'spikes'
-
-% History?
-pars.class_params.hist_bins             = 0;
-
-% Folds for multi-fold cross-validation
-pars.class_params.n_folds               = 100;
-pars.class_params.num_test_trials       = 1; % number of trials per target for test set
-
-% A couple other slightly redundant definitions
-pars.class_params.manifold              = [pars.spiking_inputs{1}(1:end-7) '_pca'];
-pars.class_params.mani_dims             = pars.mani_dims;
-
-pars.class_params.idx_start             = pars.idx_start;
-pars.class_params.idx_end               = pars.idx_end;
-pars.class_params.idx_start_classify    = {'idx_go_cue',-13};
-pars.class_params.idx_end_classify      = {'idx_go_cue',2};
-
-pars.class_params.save_fig              = false;   
-    
-    
-    
-% -------------------------------------------------------------------------
-% To estimate the "stability" of the behavior
-pars.stab_behav.signal                  = 'vel';
-pars.stab_behav.trial_avg               = false;
-pars.stab_behav.save_fig                = true;
+% Load rest of the default parameters
+params_stability_over_time;
 
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -189,51 +38,34 @@ monkey_file_lists;
 % -------------------------------------------------------------------------
 % Go to the path where the data are, and update the file list if necessary
 % (i.e., if using "unsorted" data)
-
 here                = pwd;
 
 % If we want to "unsort", choose the appropriate file names
 switch lower(pars.monkey)
     % Chewie M1-PMd
     case 'chewie'
-        %cd('/Users/juangallego/Documents/NeuroPlast/Data/Chewie')
         files = files_chewie;
     % Chewie M1 only implant
     case 'chewie2'
-        %cd('/Users/juangallego/Documents/NeuroPlast/Data/Chewie')
         files = files_chewie2;
     case 'jaco'
         files = files_jaco;
     % Mihili M1-PMd
     case 'mihili'
-        %cd('/Users/juangallego/Documents/NeuroPlast/Data/Mihili')
         files = files_mihili;
     % MrT PMd
     case 'mrt'
-        %cd('/Users/juangallego/Documents/NeuroPlast/Data/MrT')
         files = files_mrt;
     % Han S1
     case 'han'
         if ~pars.unsorted_yn
-            warning('Data for Han are not sorted'); 
-            warning('Loading threshold crossings'); 
-            pause(3);
-            pars.unsorted_yn = true;
-            %cd('/Users/juangallego/Documents/NeuroPlast/Data/Han')
-        else
-            %cd('/Users/juangallego/Documents/NeuroPlast/Data/Han')
+            warning('Data for Han are not sorted; Loading threshold crossings'); pars.unsorted_yn = true;
         end
         files = files_han;
     % Chips S1
     case 'chips'
         if ~pars.unsorted_yn
-            warning('Data for Chips are not sorted'); 
-            warning('Loading threshold crossings'); 
-            pause(3);
-            pars.unsorted_yn = true;
-            %cd('/Users/juangallego/Documents/NeuroPlast/Data/Chips')
-        else
-            %cd('/Users/juangallego/Documents/NeuroPlast/Data/Chips')
+            warning('Data for Chips are not sorted; Loading threshold crossings'); pars.unsorted_yn = true;
         end
         files = files_chips; 
 end
@@ -250,11 +82,6 @@ end
 % Sorted data
 if ~pars.unsorted_yn
     
-    % This loads and already preprocesses and does dimensionality reduction
-    % 1) Loads Baseline trials only (no force field or visuomotor
-    % adaptation); 2) Loads only successful trials; 3) Square
-    % root-transforms, smooths and does PCA (on the entire trial); 4)
-    % Downsamples the data 
     [master_td, pars_td] = loadTDfiles(  files, ...
                             {@getTDidx,'epoch','BL','result','R'}, ...
                             {@removeBadTrials,pars.bad_trial_params}, ...
@@ -268,13 +95,9 @@ if ~pars.unsorted_yn
 % Unsort the units                        
 else      
     
-    % % This loads and only does a little bit of preprocessing, with no dim
-    % reduction: 1) Loads Baseline trials only (no force field or visuomotor
-    % adaptation); 2) Loads only successful trials; 3) Merges all units in
-    % the same channel for Mihili and Chewie, but not for Han
-    if sum(strcmpi(pars.monkey,{'mihili','chewie','chewie2','mrt'}))
+    if sum(strcmpi(pars.monkey,{'mihili','chewie','chewie2','mrt','jaco'}))
 
-
+        % FOR PMd
         if strcmpi(pars.spiking_inputs{1},'PMd_spikes')
         
             [master_td, pars_td] = loadTDfiles(  files, ...
@@ -286,10 +109,12 @@ else
                             {@smoothSignals,struct('signals',pars.spiking_inputs,'calc_fr',true,'kernel_SD',pars.kernel_SD)}, ...
                             {@trimTD,{'idx_target_on',0},{'idx_trial_end',0}}, ...
                             {@getPCA,struct('signals',pars.spiking_inputs)});
-                        
+        
+        % FOR M1                        
         elseif strcmpi(pars.spiking_inputs{1},'M1_spikes')
             
-            if ~strcmpi(pars.monkey,'chewie2')
+            % For Chewie and Mihili things work without removing any trials
+            if strcmpi(pars.monkey,'chewie') || strcmpi(pars.monkey,'mihili')
                 
                 [master_td, pars_td] = loadTDfiles(  files, ...
                             {@getTDidx,'epoch','BL','result','R'}, ...
@@ -302,10 +127,8 @@ else
                             {@getPCA,struct('signals',pars.spiking_inputs)}, ...
                             {@trimTD,pars.idx_start,pars.idx_end});
                         
-            % For Chewie2: we have to remove some trials for which end time
-            % goes beyong trial time, and some others for which requested
-            % start time is <1
-            elseif strcmpi(pars.monkey,'chewie2')
+            % For Chewie2 and Jaco we need to remove some trials 
+            elseif strcmpi(pars.monkey,'chewie2') || strcmpi(pars.monkey,'jaco')
          
                 % request minimum movement time and delay between trial
                 % start and movement onset
@@ -325,15 +148,8 @@ else
                             {@sqrtTransform,pars.spiking_inputs}, ...
                             {@smoothSignals,struct('signals',pars.spiking_inputs,'calc_fr',true,'kernel_SD',pars.kernel_SD)}, ...
                             {@trimTD,{'idx_target_on',0},{'idx_trial_end',0}}, ...
-                            {@getPCA,struct('signals',pars.spiking_inputs)}); 
-                
-%                 % I have to remove params again, because I'm dumb and can't
-%                 % do it in one step above -- but it works
-%                 bp = struct('ranges',{{'idx_target_on','idx_movement_on',[5 Inf]}},'remove_nan_idx',false);
-%                 master_td = removeBadTrials(master_td,bp);
-                
-                % Now trim
-                master_td = trimTD( master_td, pars.idx_start, pars.idx_end );
+                            {@getPCA,struct('signals',pars.spiking_inputs)}, ...
+                            {@trimTD, pars.idx_start, pars.idx_end} );
             end 
         end
 
@@ -347,11 +163,7 @@ else
 end
 
 % Keep PCA pars, get rid of the rest of the extra outputs
-if strcmpi(pars.monkey,'chewie2')
-    for s = 1:size(pars_td.extra_outs,1)
-        pca_info(s) = pars_td.extra_outs{s,end};
-    end
-elseif strcmpi(pars.spiking_inputs{1},'PMd_spikes')
+if strcmpi(pars.spiking_inputs{1},'PMd_spikes')
     for s = 1:size(pars_td.extra_outs,1)
         pca_info(s) = pars_td.extra_outs{s,end};
     end
@@ -359,13 +171,13 @@ elseif ~( strcmpi(pars.monkey,'han') || strcmpi(pars.monkey,'chips') )
     for s = 1:size(pars_td.extra_outs,1)
         pca_info(s) = pars_td.extra_outs{s,end-1};
     end
-clear pars_td s;
 end
+clear pars_td s;
 
 
 % go back to where you were path-wise
 cd(here);
-clear files* here;
+clear files* here min_* i;
 
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -391,6 +203,7 @@ if sum( i_sort - 1:length(i_sort) ) > 0
         meta.sessions{s} = datestr(sorted_dates(s),'mm-dd-yyyy');
     end
 end
+clear i_sort s sorted_dates;
 
 
 % -------------------------------------------------------------------------
